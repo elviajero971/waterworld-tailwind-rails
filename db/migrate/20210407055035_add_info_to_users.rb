d0a8e935a32e6e08b310fb72add201f68e817da2
elviajero971 <lucas.illiano@hotmail.com>
2021-04-07 08:39:21 +0200

    first commit

---
+++ db/migrate/20210407055035_add_info_to_users.rb
@@ -0,0 +1,8 @@
+class AddInfoToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    add_column :users, :slug, :string
+    add_index :users, :slug, unique: true
+  end
+end
---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+// postcss.config.js
+
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require('postcss-import'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    })
+  ]
+}
+
