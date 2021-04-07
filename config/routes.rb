6c132cba26ee7903adea4d971cc7aee983ebd983
elviajero971 <lucas.illiano@hotmail.com>
2021-04-07 10:13:56 +0200

    adding category attractions and active storage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :attractions
+  resources :titles
+  resources :categories
   resources :user, only: [:show, :update, :edit]
   devise_for :users
   get 'home/index'
-  root 'home#index'
+  root 'attractions#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210407064840_drop_table_title.rb
@@ -0,0 +1,4 @@
+class DropTableTitle < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
